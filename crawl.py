342f6b2873396906b96c1e7629d7b125ec2b2963
hua <1183855934@qq.com>
2017-06-12 22:13:49 +0800

    第二次修改

--- crawl.py
+++ crawl.py
@@ -2,6 +2,15 @@
 
 import pandas as pd
 import numpy as np
+import requests
 
 var = pd.Series(range(5))
-print(var)
+url = 'http://www.liaoxuefeng.com/wiki/0013739516305929606dd18361248578c67b8067c8c017b000/0013743858312764dca7ad6d0754f76aa562e3789478044000'
+
+def getHTMLText(url):
+	r = requests.get(url)
+	return r.text
+	
+
+html = getHTMLText(url)
+print(html[:1000])
